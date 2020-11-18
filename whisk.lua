365b2607cca8781f4abed63ecd31911672d6fcd0
Kjetil Flovild-Midtlie <kjetil.flovild@gmail.com>
2020-11-18 20:41:08 +0100

    working on a plan

--- whisk.lua
+++ whisk.lua
@@ -1,5 +1,5 @@
 require "pl"
-hc = require('httpclient')
+-- hc = require('httpclient')
 
 -- early
 function access(a)
@@ -13,18 +13,42 @@ endpoints={}
 -- better
 function endpoint(data)
 	idx = table.getn(endpoints) + 1
-	io.write(idx .. ":")
-	pretty.dump(data)
+	--io.write(idx .. ":")
+	--pretty.dump(data)
 	assert( data[1] , "No URL for " .. idx )
 	endpoints[data[1]] = data
 	endpoints[data[1]][1] = nil
+	return endpoints[data[1]]
 end
 
-dofile("access")
+tasks={}
+function task(data)
+	--pretty.dump(data)
+	idx = table.getn(tasks) + 1
 
+	assert( data[1] , "No name on task no." .. idx )
+	tasks[data[1]] = data
+end
+
+function http_task(data)
+	data["__schema"] = "http-client"
+	task(data)
+end
+
+function aws_task(data)
+	data["__schema"] = "aws"
+	task(data)
+end
+
+dofile("Whiskfile")
+
+io.write("---endpoints--\n")
 pretty.dump(endpoints)
+io.write("---tasks--\n")
 
-hc_ = hc.new()
-res = hc_:head('http://httpbin.org/get')
+pretty.dump(tasks)
 
-pretty.dump(res)
+-- hc_ = hc.new()
+-- res = hc_:head('http://httpbin.org/get')
+
+-- pretty.dump(res)
