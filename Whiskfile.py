3f982d071d1d6d5bb725910d8721e4322542255f
Kjetil Flovild-Midtlie <kjetil.midtlie@gmail.com>
2021-05-12 17:50:34 +0200

    batshit crazy testing it out

--- Whiskfile.py
+++ Whiskfile.py
@@ -8,12 +8,30 @@ class Task:
 registry: List[Callable] = []
 
 
-def register(fn: Callable) -> Callable:
-    registry.append(fn)
-    return fn
 
-def inp(dep_func: Callable) -> Callable:
-    val = dep_func()
+# BATCH SHIT crazy framework DSL
+
+
+
+def framework(id,opts):
+    """ the smarness here """
+    return id,opts
+
+def resource(id):
+    def decor(fn: Callable) -> Callable:
+        registry.append(fn)
+        def wrapper(*args,**kwargs):
+            options = fn(*args,**kwargs)
+            # merge options
+            mix = {"shit":"out", **options}
+            return framework(id,mix)
+        return wrapper
+    return decor
+
+    return wrapper
+
+def inp(*args) -> Callable:
+    val = [fn() for fn in args]
     print("DEP_FUNC val = ",val)
     def decor (fn: Callable) -> Callable:
       def wrapper(*args, **kwargs):
@@ -23,29 +41,26 @@ def inp(dep_func: Callable) -> Callable:
     return decor
 
 
-@register
+@resource("aws.ssm.param")
 def fetch_root_pwd() -> Task:
     print("aws ssm get PWD")
-    return "password"
+    return { "key":"/pwd"}
 
-@register
+@resource("aws.ssm.param")
 @inp(fetch_root_pwd)
 def fetch_url(inp) -> Task:
     print("aws ssm geT url",inp)
-    return "http://B"
+    return {"key":"/A"}
 
-@register
+@resource("aws.ssm.param")
 @inp(fetch_root_pwd)
 def fetch_url2(inp) -> Task:
     print("aws ssm geT url",inp)
-    return "http://A"
+    return {"key":"/B"}
 
-@register
-@inp(fetch_url)
-@inp(fetch_url2)
+@inp(fetch_url,fetch_url2)
 def exec(inp) -> Task:
-    y = fetch_url()
-    print("aws ssm get-",y)
+    print("aws ssm get-",inp)
     return {}
 
 
